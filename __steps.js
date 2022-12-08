12a8afe9af50b2609cc51613bcfbf80eaaa5939a
najmusskb <najmussakib173@gmail.com>
2022-12-08 18:43:44 +0600

    backend api hit

---
+++ __steps.js
@@ -0,0 +1,12 @@
+/* 
+POST 
+1.CREATE POST API
+2.use fetch with method post 
+3.add header to the fetch 
+4.add body to the fetch to sent data 
+
+
+
+
+
+*/
--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 // Make express server 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 app.get('/', (req, res) => {
-
     res.send('simple node server running ');
-
 });
+
+app.use(cors());
+app.use(express.json());
+
+const users = [
+
+    { id: 1, name: 'sakib', email: 'sakib173@gmail.com' },
+    { id: 2, name: 'sabana', email: 'sabana@gmail.com' },
+    { id: 3, name: 'sabnum', email: 'sabnum@gmail.com' },
+
+];
+
+
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+
+
+
+app.post('/users', (req, res) => {
+    console.log('post api call')
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    console.log(user)
+    res.send(user);
+})
+
+
+
 app.listen(port, () => {
-
     console.log(`simple node server on running on port ${port} `);
-
-
 })
