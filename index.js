f5c7aae9e9dc24b4796306dcc17696bbcf3571fe
najmusskb <najmussakib173@gmail.com>
2022-12-08 13:34:41 +0600

    node express make

---
+++ index.js
@@ -0,0 +1,15 @@
+// Make express server 
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+
+    res.send('simple node server running ');
+
+});
+app.listen(port, () => {
+
+    console.log(`simple node server on running on port ${port} `);
+
+
+})
